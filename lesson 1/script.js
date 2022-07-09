2a85d5ff02f281607b6117d17963de7fce1fef79
mane2004 <109000037+mane2004@users.noreply.github.com>
2022-07-10 01:43:26 +0400

    Add files via upload

---
+++ lesson 1/script.js
@@ -0,0 +1,71 @@
+//----25
+
+// let num = 867;
+// if(7 = 8 + 6){
+//     console.log("yes")
+// }
+// else {
+//     console.log("no")
+// }
+
+
+//-----27
+
+// let num = 830;
+// if(8 == 3 || 8 == 0 || 0 == 3 ){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+
+//-----28
+
+// let num = 736;
+// if(0 == (7+3+6)%2){
+//     console.log("0")
+// }
+// else{
+//     console.log("1")
+// }
+
+
+//--30
+
+// let num = 539;
+// if(5>3 && 5>9){
+//     console.log("5")
+// }
+// else if(3>5 && 3>9){
+//     console.log("3")
+// }
+// else{
+//     console.log("9")
+// }
+
+//-----31
+
+// let num =974;
+// if(9<7 && 9<4){
+//     console.log("9")
+// }
+// else if(7<9 && 7<4){
+//     console.log("7")
+// }
+// else{
+//     console.log("4")
+// }
+
+
+//-----32
+
+// let num = 649;
+// if(9>4){
+//     console.log((6+4+9)/649)
+// }
+// else{
+//     console.log(649/(6+4+9))
+// }
+
+
