4aa2f2372b666c2cb0e7d94747917a6a787ca191
mane2004 <109000037+mane2004@users.noreply.github.com>
2022-07-10 00:24:20 +0400

    Add files via upload

---
+++ lesson 1/script.js
@@ -0,0 +1,271 @@
+// ---------------------   Xndir 25
+
+// let a = prompt("in poot numner");
+// // console.log(a[0]);
+// // console.log(a[1]);
+// // console.log(a[2]);
+// // console.log(a[0]+a[1]+a[2]);
+// let b = Number(a[2]);
+// if(b ==  Number(a[0])+Number(a[1]) ){
+//     console.log("yes");a
+// }
+// else if(b != Number(a[0])+Number(a[1])){
+//     console.log("no");
+// }
+
+//-----------------------  Xndir 27
+
+
+// let a = prompt("in number");
+// if(Number(a[0]) == Number(a[1])){
+//     console.log("yes");
+// }
+// else if(Number(a[0]) == Number(a[2])){
+//     console.log("yes");
+// }
+// else if(Number(a[2]) == Number(a[1])){
+//     console.log("yes");
+// }
+// else if(Number(a[0]) != Number(a[1])){
+//     console.log("no")
+// }
+// else if(Number(a[0]) != Number(a[2])){
+//     console.log("no")
+// }
+// else if(Number(a[1]) != Number(a[2])){
+//     console.log("no")
+// }
+
+
+//-------------------   Xndir 28
+
+
+
+// let a = prompt("in number");
+// let b = Number(a[0]+Number(a[1])+Number(a[2]));
+// if(1 == b % 2){
+//     console.log("1")
+// }
+// else if(0 == b % 2){
+//     console.log("0")
+// }
+
+
+
+//---------------------------- Xndir 30
+
+
+// let a = prompt("in number");
+// if(Number(a[0]) > Number(a[1]),Number(a[0]) > Number(a[2])){
+//     console.log(a[0])
+// }
+// else if(Number(a[1]) > Number(a[0]),Number(a[1]) > Number(a[2])){
+//     console.log(a[1])
+// }
+
+
+
+// // function maxdigit(number){
+//    let a = Math.floor(number%10);
+//     let b = Math.floor(number%100/10);
+//     let c = Math.floor(number/100);
+//     // return Math.max(a,b,c)
+// // }
+// if(a>b & a>c){
+// console.log(a)
+// }
+// else if(b>a & b>c){
+// console.log(b)
+// }
+// else if(c>a & c>b){
+// console.log(c)
+// }
+
+
+ //---------------------------- xndir 25
+
+// let d = prompt("in number");
+// let a =  ((d-d%100)/100);
+// let b = ((d-d%10)%100/10);
+// let c = (d%10);
+// if(c == a+b){
+//    console.log("yes")
+// }
+// else if(c != a+b){
+//    console.log("no")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------- xndir 27
+
+// let d = prompt("in number");
+// let a = ((d-d%100)/100);
+// let b = ((d-d%10)%100/10);
+// let c = (d%10);
+// if(a == b || a == c || b ==c){
+//    console.log("no")
+// }
+// else if(a != b || a != c || c != b){
+//    console.log("yes")
+// }
+
+
+//------------------------ xndir 28
+
+// let d = prompt("in number");
+// let a = ((d-d%100)/100);
+// let b = ((d-d%10)%100/10);
+// let c = (d%10);
+// let z = a + b + c; 
+// // if(1 == z%2){
+// //    console.log(1)
+// // }
+// // else if(0 == z%2){
+// //    console.log(0)
+// // }
+
+//------------------------ xndir 30
+
+
+// let d = prompt("in number");
+// let a = (d%10);
+// let b = ((d-d%10)%100/10);
+// let c = ((d-d%100)/100);
+// let n = Math.max(a,b,c);
+// console.log(n)
+
+//---------------------- xndir 31
+
+
+// let d = prompt("in number");
+// let a = (d%10);
+// let b = ((d-d%10)%100/10);
+// let c = ((d-d%100)/100);
+// let n = Math.min(a,b,c);
+// console.log(n)
+
+
+//------------------------ xndir 32
+
+// let d = prompt("in number");
+// let a = (d%10);
+// let b = ((d-d%10)%100/10);
+// let c = ((d-d%100)/100);
+// if(a>b){
+//     console.log((a + b + c)/d)
+// }
+// else if(a < b){
+//     console.log(d)
+// } 
+
+//------------------- xndir 26
+
+// let a = prompt('in number');
+// if(a >=10 & a < 100){
+//     console.log("2")
+// }
+// else if(a >=100 & a < 1000){
+//     console.log("3")
+// }
+// else if(a < 10 || a > 100){
+//     console.log("0")
+// }
+
+//--------------------- xndir 29
+
+// let a = prompt("in number");
+// if(a > 500){
+//     console.log(a / (a%10 + ((a-a%10)%100/10) + (a-a%100)/100))
+// }   
+// else if(a < 500){
+//     console.log((a%10 + ((a-a%10)%100/10) + (a-a%100)/100) / a)
+//}
+
+
+
+//----25
+
+// let num = 867;
+// if(7 = 8 + 6){
+//     console.log("yes")
+// }
+// else {
+//     console.log("no")
+// }
+
+
+//-----27
+
+// let num = 830;
+// if(8 == 3 || 8 == 0 || 0 == 3 ){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+
+//-----28
+
+// let num = 736;
+// if(0 == (7+3+6)%2){
+//     console.log("0")
+// }
+// else{
+//     console.log("1")
+// }
+
+
+//--30
+
+// let num = 539;
+// if(5>3 && 5>9){
+//     console.log("5")
+// }
+// else if(3>5 && 3>9){
+//     console.log("3")
+// }
+// else{
+//     console.log("9")
+// }
+
+//-----31
+
+// let num =974;
+// if(9<7 && 9<4){
+//     console.log("9")
+// }
+// else if(7<9 && 7<4){
+//     console.log("7")
+// }
+// else{
+//     console.log("4")
+// }
+
+
+//-----32
+
+// let num = 649;
+// if(9>4){
+//     console.log((6+4+9)/649)
+// }
+// else{
+//     console.log(649/(6+4+9))
+// }
+
